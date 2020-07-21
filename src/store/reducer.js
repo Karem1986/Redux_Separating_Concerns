2775363d8c1695c5f9e30bdf5534be027d3acbdf
Karem1986 <karem.ortiz@outlook.com>
2020-07-21 10:13:15 +0200

    created store, added reducer and provider

---
+++ src/store/reducer.js
@@ -0,0 +1,13 @@
+// src/store/reducer.js
+const initialState = {
+    // TODO
+    hello: "world"
+};
+
+export default function reducer(state = initialState, action) {
+    switch (action.type) {
+        default: {
+            return state;
+        }
+    }
+}
